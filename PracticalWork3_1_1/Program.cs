13113c96fe68f0820c9c104f308aeb45087311d0
Ilya Konst <mendellq@gmail.com>
2025-10-23 18:54:29 +0300

    third practicalwork

---
+++ PracticalWork3_1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticalWork3_1_1;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine());
+        int reversed = ReverseNumber(n, 0);
+        Console.WriteLine($"Разворот числа: {reversed}");
+    }
+    
+    static int ReverseNumber(int n, int result)
+    {
+        if (n == 0)
+            return result;
+
+        return ReverseNumber(n / 10, result * 10 + n % 10);
+    }
+}
---
+++ PracticalWork3_1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticalWork3_1_2;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите m: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+        Console.WriteLine($"A({m},{n}) = {result}");
+    }
+    
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (m > 0 && n == 0)
+            return Ackermann(m - 1, 1);
+        else // m > 0 && n > 0
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
